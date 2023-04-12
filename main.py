8fc51fe92f725d2d7797970d80b3ec26cb07f1fe
adriencr81 <107180086+adriencr81@users.noreply.github.com>
2023-04-12 16:09:31 +0200

    creation map et affichage

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 import pygame
-pygame.init()
-
-# creer la fenetre du jeu
-pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Pokepython - Aventure")
-
-# boucle du jeu
-running = True
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-pygame.quit()
+from game import Game
+if __name__ == '__main__':
+    pygame.init()
+    game = Game()
+    game.run()
