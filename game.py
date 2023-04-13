a4724e4b4ea576043d0928b31976542f10d0d49b
adriencr81 <107180086+adriencr81@users.noreply.github.com>
2023-04-13 12:36:35 +0200

    ajout personnage

--- game.py
+++ game.py
@@ -2,6 +2,8 @@ import pygame
 import pytmx
 import pyscroll
 
+from player import Player
+
 class Game:
 
     def __init__(self): 
@@ -15,20 +17,54 @@ class Game:
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
         map_layer.zoom = 2
 
+
+        # generer un joueur 
+        player_position = tmx_data.get_object_by_name("player")
+        self.player = Player(player_position.x, player_position.y)
+
         # dessiner le groupe de calques
-        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=1)
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=3)
+        self.group.add(self.player)
+
+
+    def handle_input(self):
+        pressed = pygame.key.get_pressed()
+
+        if pressed[pygame.K_UP]:
+            self.player.move_up()
+            self.player.change_animation('up')
+        elif pressed[pygame.K_DOWN]:
+            self.player.move_down()
+            self.player.change_animation('down')
+        elif pressed[pygame.K_LEFT]:
+            self.player.move_left()
+            self.player.change_animation('left')
+        elif pressed[pygame.K_RIGHT]:
+            self.player.move_right()
+            self.player.change_animation('right')
+
+    
+
 
     def run(self):
+
+        clock = pygame.time.Clock()
+
         # boucle du jeu
         running = True
 
         while running:
 
+            self.handle_input()
+            self.group.update()
+            self.group.center(self.player.rect)
             self.group.draw(self.screen)
             pygame.display.flip()
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+
+            clock.tick(60)        
 
         pygame.quit()
