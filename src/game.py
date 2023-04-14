04d43b0d60a84d7dc82f46674c00e1a2ac55ff98
adriencr81 <107180086+adriencr81@users.noreply.github.com>
2023-04-14 12:14:43 +0200

    factorisation code

--- src/game.py
+++ src/game.py
@@ -1,8 +1,10 @@
 import pygame
 import pytmx
 import pyscroll
+from map import MapManager
 
 from player import Player
+
 
 class Game:
 
@@ -11,21 +13,12 @@ class Game:
         self.screen = pygame.display.set_mode((800, 600))
         pygame.display.set_caption("Pokepython - Aventure")
 
-        #charger la carte (tmx)
-        tmx_data = pytmx.util_pygame.load_pygame('map.tmx')
-        map_data = pyscroll.data.TiledMapData(tmx_data)
-        map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
-        map_layer.zoom = 2
-
 
         # generer un joueur 
-        player_position = tmx_data.get_object_by_name("player")
-        self.player = Player(player_position.x, player_position.y)
+        self.player = Player(0, 0)
+        self.map_manager = MapManager(self.screen, self.player)
 
-        # dessiner le groupe de calques
-        self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=3)
-        self.group.add(self.player)
-
+   
 
     def handle_input(self):
         pressed = pygame.key.get_pressed()
@@ -44,7 +37,11 @@ class Game:
             self.player.change_animation('right')
 
     
+    
+    def update(self):
+        self.map_manager.update()
 
+       
 
     def run(self):
 
@@ -55,10 +52,10 @@ class Game:
 
         while running:
 
+            self.player.save_location()
             self.handle_input()
-            self.group.update()
-            self.group.center(self.player.rect)
-            self.group.draw(self.screen)
+            self.update()
+            self.map_manager.draw()
             pygame.display.flip()
 
             for event in pygame.event.get():
--- src/player.py
+++ src/player.py
@@ -4,7 +4,7 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self, x, y):
         super().__init__()
-        self.sprite_sheet = pygame.image.load('player.png')
+        self.sprite_sheet = pygame.image.load('sprites/player.png')
         self.image = self.get_image(0, 0)
         self.image.set_colorkey([0, 0, 0])
         self.rect = self.image.get_rect()
@@ -15,8 +15,12 @@ class Player(pygame.sprite.Sprite):
             'right': self.get_image(0, 64),
             'up': self.get_image(0, 96),
         }
+        self.feet = pygame.Rect(0, 0, self.rect.width * 0.5, 12)
+        self.old_position = self.position.copy()
         self.speed = 2
 
+    def save_location(self): self.old_position = self.position.copy()
+    
     def change_animation(self, name):
         self.image = self.images[name]
         self.image.set_colorkey((0, 0, 0))
@@ -31,7 +35,13 @@ class Player(pygame.sprite.Sprite):
 
     def update(self):
         self.rect.topleft = self.position
+        self.feet.midbottom = self.rect.midbottom
 
+    def move_back(self):
+        self.position = self.old_position
+        self.rect.topleft = self.position
+        self.feet.midbottom = self.rect.midbottom
+    
     def get_image(self, x, y):
         image = pygame.Surface([32, 32])
         image.blit(self.sprite_sheet, (0, 0), (x, y, 32, 32))
